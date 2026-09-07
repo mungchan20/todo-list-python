5f9baeeb8352c155418944b92593cb2696df5802
mungchan <85596908+mungchan20@users.noreply.github.com>
2026-09-07 12:20:32 +0900

    클래스로 리팩토링

--- 1.todolist.py
+++ 1.todolist.py
@@ -1,28 +1,29 @@
-# 할 일 추가하기
-def add_todo(todos,content): # 매개변수는 전달받는 통로
-    todos.append({"할일":content,"완료":False})
+class TodoList:
+    def __init__(self):
+        self.todos = []
 
-# 할 일 목록보기
-def show_todos(todos):
-    num = 1
-    for todo in todos:
-        if todo["완료"] == False:
-            print(f"{num}.[] {todo['할일']}")
-        else:
-            print(f"{num}.[V] {todo['할일']}")
-        num += 1
+    def add_todo(self,content):
+        self.todos.append({"할일":content,"완료":False})
 
-# 할 일 완료하기
-def complete_todo(todos,num):
-    todos[num -1]["완료"] = True
+    def show_todos(self):
+        num = 1
+        for todo in self.todos:
+            if todo["완료"] == False:
+                print(f"{num}.[] {todo['할일']}")
+            else:
+                print(f"{num}.[V] {todo['할일']} ")
+            num += 1
 
-# 할 일 삭제하기
-def delete_todo(todos,num):
-    del todos[num-1]
+    def complete_todo(self,num):
+        self.todos[num-1]["완료"] = True
+
+    def delete_todo(self,num):
+        del self.todos[num-1]
+
 
 # 설계
 print("todo 리스트에 오신 것을 환영합니다.")
-todos = []
+todo_list = TodoList()
 
 while True:
     print(f"1.할 일 추가하기\n2.할 일 목록보기\n3.할 일 완료하기\n4.할 일 삭제하기\n5.종료하기")
@@ -30,19 +31,19 @@ while True:
 
     if menu == 1:
         content = input("추가하실 할 일을 입력하세요:")
-        add_todo(todos,content)
+        todo_list.add_todo(content)
     elif menu == 2:
-        show_todos(todos)
+        todo_list.show_todos()
     elif menu == 3:
         num = int(input("완료하실 할 일의 번호를 입력하세요"))
-        if 1 <= num <= len(todos):
-            complete_todo(todos,num)
+        if 1 <= num <= len(todo_list.todos):
+            todo_list.complete_todo(num)
         else:
             print("번호를 제대로 입력하십시오.")
     elif menu == 4:
         num = int(input("삭제하실 할 일의 번호를 입력하세요"))
-        if 1 <= num <= len(todos):
-            delete_todo(todos,num)
+        if 1 <= num <= len(todo_list.todos):
+            todo_list.delete_todo(num)
         else:
             print("번호 좀 똑바로 쳐라")
     elif menu == 5:
